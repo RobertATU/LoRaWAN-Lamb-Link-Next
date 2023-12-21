2f386cb7432c086bc854bd52242a3b4a094aae14
Robert <g00395612@atu.ie>
2023-12-21 13:36:21 +0000

    Initial Commit

--- frontend/pages/api/new-pin.js
+++ frontend/pages/api/new-pin.js
@@ -1,6 +1,6 @@
 
 async function handler(req, res) { 
-  const response = await fetch('http://localhost:8000/createNew', {
+  const response = await fetch('http://localhost:8080/api/pins/addPin', {
     method: 'POST',
     body: JSON.stringify(req.body),
     headers: {

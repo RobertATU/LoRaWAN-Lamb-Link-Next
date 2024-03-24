88b90061c95f8cc490b349ee1414c44ee5514e4b
Robert <g00395612@atu.ie>
2024-03-24 11:49:28 +0000

    Improved messaging

--- spring-backend/src/main/java/ie/atu/springbackend/PinsService.java
+++ spring-backend/src/main/java/ie/atu/springbackend/PinsService.java
@@ -1,20 +1,48 @@
 package ie.atu.springbackend;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Optional;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.text.SimpleDateFormat;
+
+import com.twilio.Twilio;
+import com.twilio.converter.Promoter;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import java.net.URI;
+import java.math.BigDecimal;
 
 @Service
 public class PinsService {
-
+    // Find your Account Sid and Token at twilio.com/console
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
+    public int count, upsideDown;
     @Autowired
     private PinsRepo pinsRepo;
     public List<Pins>allPins(){
-        System.out.println(pinsRepo.findAll());
-        return pinsRepo.findAll();
+        List<Pins> pinsList = pinsRepo.findAll();
+        SimpleDateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy, HH:mm:ss");
+
+            for(Pins pins : pinsList) {
+                System.out.println(pins.getId().getDate());
+
+                pins.setDate(outputFormat.format(pins.getId().getDate()));
+                pins.setGenId(pins.getId().toHexString());
+            }
+        return pinsList;
     }
 
     public Optional<Pins> findPinsById(String sheepId) {
@@ -24,6 +52,54 @@ public class PinsService {
 
     public Pins addPins(Pins pins) {
         System.out.println("Service:"+pins);
+        System.out.println("work");
+        System.out.println(pins);
+        System.out.println(pins.getObjectJSON());
+        System.out.println(pins.getDeviceName());
+        System.out.println(pins.getDevEUI());
+
+
+        String jsonString = pins.getObjectJSON();
+        JSONObject json = new JSONObject(jsonString);
+
+
+        pins.setLongitude(json.getDouble("longitude"));
+        pins.setLatitude(json.getDouble("latitude"));
+        pins.setSheepId(json.getString("name"));
+        pins.setAccelero_x(json.getDouble("accelero_x"));
+        if(pins.getAccelero_x() < 0){
+            count++;
+            System.out.println("Count" + count);
+        }
+        else if(pins.getAccelero_x() > 0 &&count > 4){
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Message message = Message.creator(
+                    new com.twilio.type.PhoneNumber("whatsapp:+353877178072"),
+                    new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                    "Your Sheep is back up").create();
+
+            System.out.println(message.getSid());
+
+            count = 0;
+            System.out.println("Count" + count);
+        }
+        else {
+
+            count = 0;
+            System.out.println("Count" + count);
+        }
+
+        if(count == 5){
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Message message = Message.creator(
+                    new com.twilio.type.PhoneNumber("whatsapp:+353877178072"),
+                    new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                    "Your Sheep is upside down").create();
+
+            System.out.println(message.getSid());
+
+        }
+        System.out.println(pins);
         return pinsRepo.save(pins);
     }
 
